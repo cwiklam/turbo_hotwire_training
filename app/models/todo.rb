69d4bb1085c29b624b868c25ed3ee1cbc86cecee
cwiklam <cwikla.m0@gmail.com>
2023-04-01 11:55:21 +0200

    Fixed clear input

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,7 +2,6 @@ class Todo < ApplicationRecord
   after_commit :publish_todo, on: :create
 
   def publish_todo
-    broadcast_replace_to 'todos_stream', target: 'new_todo', partial: "todos/form", locals: { todo: Todo.new, frame_id: 'new_todo' }
     broadcast_append_to 'todos_stream', target: 'todos_list', partial: 'todos/item', locals: { todo: self }
   end
 end
