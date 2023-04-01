30d27044d8bf4a43f36304c872a2080804cb9fa6
cwiklam <cwikla.m0@gmail.com>
2023-04-01 11:52:53 +0200

    Adding - refreshing on all opened cards

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,7 +1,8 @@
 class Todo < ApplicationRecord
-  #after_commit :publish_todo, on: :create
+  after_commit :publish_todo, on: :create
 
   def publish_todo
-    broadcast_update_to 'todos_stream', target: 'todos_list', partial: 'todos/list', locals: { todos: Todo.all}
+    broadcast_replace_to 'todos_stream', target: 'new_todo', partial: "todos/form", locals: { todo: Todo.new, frame_id: 'new_todo' }
+    broadcast_append_to 'todos_stream', target: 'todos_list', partial: 'todos/item', locals: { todo: self }
   end
 end
